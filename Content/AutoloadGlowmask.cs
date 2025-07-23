c51ea27779ab20df620eda382899088cf155b2fe
mimig298 <miguelldvmendes@gmail.com>
2025-07-23 19:01:20 +0100

    more documentation changes

--- Content/AutoloadGlowmask.cs
+++ Content/AutoloadGlowmask.cs
@@ -4,7 +4,7 @@ namespace GlowmaskHelper.Content;
 
 /// <summary>
 /// This attribute annotates a <see cref="Terraria.ModLoader.ModItem"/> or <see cref="Terraria.ModLoader.ModNPC"/> class to indicate that the game should autoload a glowmask texture for this entity.
-/// <para/> The glowmask texture will be autoloaded from <see cref="Terraria.ModLoader.ModItem.Texture"/> + "_Glow". An error will be thrown if this texture is not found.
+/// <para/> The glowmask texture will be autoloaded from the regular texture + "_Glow". An error will be thrown if this texture is not found.
 /// </summary>
 [AttributeUsage(AttributeTargets.Class)]
 public class AutoloadGlowmask : Attribute
