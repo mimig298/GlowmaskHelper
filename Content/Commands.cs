5c7f89f63f353275620017c4f467ccf4edaa7e78
mimig298 <miguelldvmendes@gmail.com>
2025-07-27 16:43:53 +0100

    List glowmasks command

---
+++ Content/Commands.cs
@@ -0,0 +1,35 @@
+﻿using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace GlowmaskHelper.Content;
+
+internal class ListGlowmasks : ModCommand
+{
+    public static LocalizedText DescriptionText { get; private set; }
+
+    public override void SetStaticDefaults()
+    {
+        DescriptionText = Language.GetText("GlowmaskHelper.Commands.ListGlowmasks.Description");
+    }
+
+    public override string Command => "listglowmasks";
+    public override string Description => DescriptionText.Value;
+
+    public override CommandType Type => CommandType.Chat;
+
+    public override void Action(CommandCaller caller, string input, string[] args)
+    {
+        int moddedGlowmaskCount = GlowmaskLoader.GlowmaskCount - GlowmaskLoader.VanillaGlowmaskCount;
+        if (moddedGlowmaskCount == 0)
+        {
+            caller.Reply("No glowmasks found.");
+            return;
+        }
+        caller.Reply($"Found {moddedGlowmaskCount} glowmasks:");
+
+        foreach (string path in GlowmaskLoader.glowmasks.Keys)
+        {
+            caller.Reply($"* {path}");
+        }
+    }
+}
