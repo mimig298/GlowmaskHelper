aec263df1c3ab2dcb9fd7f1d16dbb6314ac90b07
mimig298 <miguelldvmendes@gmail.com>
2025-08-01 18:41:26 +0100

    Wing support

--- Content/PlayerGlowmaskLayers.cs
+++ Content/PlayerGlowmaskLayers.cs
@@ -67,3 +67,31 @@ internal class ArmGlowmaskLayer : PlayerDrawLayer
         drawInfo.DrawDataCache.Add(data);
     }
 }
+
+internal class WingsGlowmaskLayer : PlayerDrawLayer
+{
+    public override bool GetDefaultVisibility(PlayerDrawSet drawInfo)
+    {
+        return !drawInfo.drawPlayer.dead && !drawInfo.hideEntirePlayer && drawInfo.drawPlayer.wings > 0;
+    }
+
+    public override Position GetDefaultPosition() => new AfterParent(PlayerDrawLayers.Wings);
+
+    protected override void Draw(ref PlayerDrawSet drawInfo)
+    {
+        short glowmaskSlot = GlowmaskLoader.GetGlowmaskSlot_Equip(EquipType.Wings, drawInfo.drawPlayer.wings);
+        if (glowmaskSlot <= GlowmaskLoader.VanillaGlowmaskCount)
+            return; // Don't draw glowmask if it is a vanilla one.
+        Asset<Texture2D> glowmaskTexture = TextureAssets.GlowMask[glowmaskSlot];
+
+        // Copied from PlayerDrawLayers.DrawPlayer_09_Wings():
+        Vector2 directions = drawInfo.drawPlayer.Directions;
+        Vector2 positionOffset = new(0f, 7f);
+        Vector2 position = drawInfo.Position - Main.screenPosition + new Vector2(drawInfo.drawPlayer.width / 2, drawInfo.drawPlayer.height - drawInfo.drawPlayer.bodyFrame.Height / 2) + positionOffset;
+        position += new Vector2(-9, 2) * directions;
+
+        DrawData item = new(glowmaskTexture.Value, position.Floor(), new Rectangle(0, glowmaskTexture.Height() / 4 * drawInfo.drawPlayer.wingFrame, glowmaskTexture.Width(), glowmaskTexture.Height() / 4), Color.White * drawInfo.stealth * (1f - drawInfo.shadow), drawInfo.drawPlayer.bodyRotation, new Vector2(glowmaskTexture.Width() / 2, glowmaskTexture.Height() / 8), 1f, drawInfo.playerEffect)
+        { shader = drawInfo.cWings };
+        drawInfo.DrawDataCache.Add(item);
+    }
+}
