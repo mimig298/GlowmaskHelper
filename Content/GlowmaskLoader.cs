23cb51c88c1f9767c76897268e502568f5333e2b
mimig298 <miguelldvmendes@gmail.com>
2025-08-03 19:28:50 +0100

    Clearing the dictionaries

--- Content/GlowmaskLoader.cs
+++ Content/GlowmaskLoader.cs
@@ -206,6 +206,9 @@ public static class GlowmaskLoader
         glowmasks.Clear();
         itemToGlowmask.Clear();
         npcToGlowmask.Clear();
+        equipToGlowmask.Clear();
+        equipArmsToGlowmask.Clear();
+        wallToGlowmask.Clear();
         Array.Resize(ref TextureAssets.GlowMask, VanillaGlowmaskCount);
     }
 }
